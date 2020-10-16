e726b9e079b5017c5ec39e23d04d63c4345e094a
Manuel <manuel.devito@icloud.com>
2020-10-16 17:55:56 +0200

    Snack 7

---
+++ js-snack7/main.js
@@ -0,0 +1,18 @@
+// Calcola la somma e la media dei numeri da 1 a 10.
+
+// Inizializzo la variabile somma.
+var somma = 0
+// Creo un ciclo for partendo da 1 fino a 10.
+
+for (var i = 1; i <= 10; i++) {
+//
+    somma = i + somma;
+}
+
+var media = somma / 10;
+
+
+console.log('La somma è = ' + somma);
+
+
+console.log('La media è = ' + media);
